ab44bcaacb146241082a3da81531e309e3bf4c2d
Yutaka Matsui <yutaka4730935@gmail.com>
2016-12-21 23:21:00 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!  
+  push: {
+    ios: [
+      {
+        pfx: 'cloud/BeautifulDietDev.p12',
+        bundleId: 'me.you.bundleId',
+        production: false // Dev
+      },
+      {
+        pfx: 'cloud/BeautifulDietPro.p12',
+        bundleId: 'me.you.bundleId',  
+        production: true // Prod
+      }
+    ]
+  },  
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   fileKey: process.env.FILE_KEY || '',
   restAPIKey: process.env.REST_API_KEY || '',
